4ecda22440ec4a549ea5ec68f2d33f67d11d96ec
Usuario553 <108374636+Usuario553@users.noreply.github.com>
2022-10-20 17:43:25 -0400

    Add files via upload

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -65,7 +65,12 @@ class ChartingState extends MusicBeatState
 		'Hey!',
 		'Hurt Note',
 		'GF Sing',
-		'No Animation'
+		'No Animation',
+		'Template1',
+		'Template2',
+		'Template3',
+		'Template4',
+		'Template5'
 	];
 	private var noteTypeIntMap:Map<Int, String> = new Map<Int, String>();
 	private var noteTypeMap:Map<String, Null<Int>> = new Map<String, Null<Int>>();
@@ -89,7 +94,10 @@ class ChartingState extends MusicBeatState
 		['Screen Shake', "Value 1: Camera shake\nValue 2: HUD shake\n\nEvery value works as the following example: \"1, 0.05\".\nThe first number (1) is the duration.\nThe second number (0.05) is the intensity."],
 		['Change Character', "Value 1: Character to change (Dad, BF, GF)\nValue 2: New character's name"],
 		['Change Scroll Speed', "Value 1: Scroll Speed Multiplier (1 is default)\nValue 2: Time it takes to change fully in seconds."],
-		['Set Property', "Value 1: Variable name\nValue 2: New value"]
+		['Set Property', "Value 1: Variable name\nValue 2: New value"],
+		['Flash Camera', "Value 1: Speed\nValue 2: New value"],
+		['Set CamZoom', "Value 1: CamZoom\nValue 2: if blank, it with smoothly zoom regulary, otherwise it will do an instant zoom"],
+		['template1', "Value 1: Variable name\nValue 2: New value"]
 	];
 
 	var _file:FileReference;
